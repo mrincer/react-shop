a36943db50bee6c1b6540e84bff2b81a1455f45d
Kirk Incer <kirk.incer@ahinko.com>
2022-03-24 17:03:16 -0600

    [ADDED] New Pages and it routes added. [Added] New Containers and Styles added

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import containers
 import Layout from "../containers/Layout.jsx";
-import Login from "../containers/Login.jsx";
 import RecoveryPassword from "../containers/RecoveryPassword.jsx";
+// import pages
 import Home from "../pages/Home.jsx";
 import NotFound from "../pages/NotFound.jsx";
+import Login from '../pages/Login.jsx';
+import PasswordRecovery from "../pages/PasswordRecovery.jsx";
+import SendEmail from '../pages/SendEmail.jsx';
+import NewPassword from '../pages/NewPassword.jsx';
+import MyAccount from '../pages/MyAccount.jsx';
+import CreateAccount from '../pages/CreateAccount.jsx';
+import Checkout from '../pages/Checkout.jsx';
+import Orders from '../pages/Orders.jsx';
+
+
 import "../styles/global.css";
+
+// New pass account signup checkout orders
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route
-            exact
-            path="/recovery-password"
-            element={<RecoveryPassword />}
-          />
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/password-recovery" element={<PasswordRecovery/>}/>
+          <Route exact path="/send-email" element={<SendEmail/>}/>
+          <Route exact path="/new-password" element={<NewPassword/>}/>
+          <Route exact path="/account" element={<MyAccount/>}/>
+          <Route exact path="/signup" element={<CreateAccount/>}/>
+          <Route exact path="/checkout" element={<Checkout/>}/>
+          <Route exact path="/orders" element={<Orders/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
